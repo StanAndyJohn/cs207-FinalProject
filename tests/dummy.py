ded9e64f1162ea4cdef24ff00e9a7c4855bd5da3
Johnathan Jiang <johnathan_jiang@harvard.edu>
2019-10-29 20:27:39 -0400

    travis dummy

---
+++ tests/dummy.py
@@ -0,0 +1,18 @@
+def dummy_func(a=1.0):
+    """Returns True.
+    
+    INPUTS
+    =======
+    a: 1.0
+    
+    RETURNS
+    ========
+    True
+    
+    EXAMPLES
+    =========
+    >>> dummy_func(1.0)
+    True
+    """
+    return True
+        
--- tests/testforBasicFeature.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def dummy():
-    return True
