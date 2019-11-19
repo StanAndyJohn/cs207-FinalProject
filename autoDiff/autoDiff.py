050928b577ce2c04107537136891cbf0b10faa26
Johnathan Jiang <johnathan_jiang@harvard.edu>
2019-11-19 12:03:52 -0500

    update autoDiff

--- autoDiff/autoDiff.py
+++ autoDiff/autoDiff.py
@@ -2,35 +2,35 @@ import numpy as np
 
 class Variable:
     def __init__(self, val, der=None):
-            self.val = val
-            self.name = 'x'
-            if der == None:
-                self.der = {'x': 1.0}
-            else:
-                self.der = der
+        """
+		Initialization of the Variable class, the default variable name in this milestone is x
+
+		Inputs:
+			val: variable value, int or float
+            der: (optional) variable derivative
+
+		Output:
+			Variable class
+
+	    """
+        self.val = val
+        self.name = 'x'
+        if der == None:
+            self.der = {'x': 1.0}
+        else:
+            self.der = der
     
     def __add__(self, other):
         """ 
-        Returns addition of Variable object.
+        Returns  Variable object from addition
         
-        Parameters
-        =======
-        Variable object (self)
-        Variable object (other) OR float/int (other)
+        Inputs:
+            self: Variable object
+            other: Variable object or scalar
         
-        Returns
-        =======
-        Variable object: self + other
+        Output:
+            Variable object: self + other
     
-        Examples
-        =======
-        >>> import numpy as np
-        >>> import autoDiff as ad
-        >>> x1 = ad.Variable(2)
-        >>> x2 = x1+2
-        >>> x3 = x1+x2
-        >>> print(x3.der)
-        {'x': 2.0}
         """
         try:
             der = {var: self.der[var] + other.der[var] for var in self.der.keys()}
@@ -39,9 +39,31 @@ class Variable:
             return Variable(self.val + other, der=self.der)
         
     def __radd__(self, other):
+        """ 
+        Returns Variable object from addition (from __add__ method)
+        
+        Inputs:
+            self: scalar
+            other: Variable object
+        
+        Output:
+            Variable object from __add__ method
+
+        """
         return self.__add__(other)
    
     def __sub__(self, other):
+        """ 
+        Returns Variable object from subtraction
+        
+        Inputs:
+            self: Variable object
+            other: Variable object or scalar
+        
+        Output:
+            Variable object: self - other
+    
+        """
         try:
             der = {var: self.der[var] - other.der[var] for var in self.der.keys()}
             return Variable(self.val - other.val, der=der)
@@ -49,6 +71,17 @@ class Variable:
             return Variable(self.val - other, der=self.der)
 
     def __rsub__(self, other):
+        """ 
+        Returns Variable object from subtraction
+        
+        Inputs:
+            self: scalar
+            other: Variable object
+        
+        Output:
+            Variable object: self - other
+    
+        """
         try:
             der = {var: other.der[var] - self.der[var] for var in self.der.keys()}
             return Variable(other.val - self.val, der=der)
@@ -57,6 +90,17 @@ class Variable:
             return Variable(other - self.val, der= der)
 
     def __mul__(self, other):
+        """ 
+        Returns Variable object from multiplication
+        
+        Inputs:
+            self: Variable object
+            other: Variable object or scalar
+        
+        Output:
+            Variable object: self * other
+    
+        """
         try:
             der = {var: self.der[var] * other.val + other.der[var] * self.val for var in self.der.keys()}
             return Variable(self.val * other.val, der=der)
@@ -65,22 +109,63 @@ class Variable:
             return Variable(self.val * other, der=der)
 
     def __rmul__(self, other):
+        """ 
+        Returns Variable object from multiplication (from __mul__ method)
+        
+        Inputs:
+            self: scalar
+            other: Variable object
+        
+        Output:
+            Variable object from __mul__ method
+
+        """
         return self.__mul__(other)
 
     def __truediv__(self, other):
+        """ 
+        Returns Variable object from division
+        
+        Inputs:
+            self: Variable object
+            other: Variable object or scalar
+        
+        Output:
+            Variable object: self / other
+    
+        """
         try:
-            der = {var: (self.der[var]*other.val-other.der[var]*self.val)/(other.val**2) for var in self.der.keys()}
-            return Variable(self.val/other.val, der=der)
+            if isinstance(other, int) or isinstance(other, float):
+                if int(other) == 0:
+                    raise ZeroDivisionError('Can not divide by 0')
+            elif int(other.val) == 0:
+                raise ZeroDivisionError('Can not divide by 0')
+            else:
+                der = {var: (self.der[var]*other.val-other.der[var]*self.val)/(other.val**2) for var in self.der.keys()}
+                return Variable(self.val/other.val, der=der)
         except AttributeError:
             der = {var: self.der[var] / other for var in self.der.keys()}
             return Variable(self.val/other, der=der)
 
     def __rtruediv__(self, other):
+        """ 
+        Returns Variable object from division
+        
+        Inputs:
+            self: scalar
+            other: Variable object
+        
+        Output:
+            Variable object: self / other
+    
+        """
         try:
             der = {var: (self.der[var]*other.val-other.der[var]*self.val)/(other.val**2) for var in self.der.keys()}
             return Variable(self.val/other.val, der=der)
         except AttributeError:
-            der = {var: -other/self.der[var] for var in self.der.keys()}
+            if int(self.val) == 0:
+                raise ZeroDivisionError('Can not divide by 0')
+            der = {var: -1*other/self.der[var] for var in self.der.keys()}
             return Variable(other/self.val, der= der)
     
     def __pow__(self, other):
@@ -92,9 +177,18 @@ class Variable:
         return self.__pow__(other)
 
     def __neg__(self):
+        """ 
+        Returns Variable object from negation
+        
+        Input:
+            self: Variable object
+        
+        Output:
+            Variable object: -self
+
+        """
         der = {var: -self.der[var] for var in self.der.keys()}
         return Variable(-self.val, der=der)
  
-
 
 
